a7852afdeb97afbbcef48f0f5e221d03fd0a0428
Collin Capano <collin.capano@aei.mpg.de>
2020-06-08 11:34:03 +0000

    include python 3.7 in supported versions

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         'Intended Audience :: Science/Research',
