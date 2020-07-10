7f984de5aab39b9169c568151c31d85654903d02
Richard Stiskalek <richard.stiskalek@atlas9.atlas.local>
2020-07-10 13:26:21 +0000

    undo base class change

--- epsie/proposals/base.py
+++ epsie/proposals/base.py
@@ -181,7 +181,7 @@ class BaseProposal(object):
         pass
 
     @abstractmethod
-    def jump(self, fromx, dimchange=None):
+    def jump(self, fromx):
         """This should provide random samples from the proposal distribution.
 
         Samples should be returned as a dictionary mapping parameters to
