50813ee43a4cb21269c9d36f33841b5cae04754c
Richard-Sti <richard.stiskalek@protonmail.com>
2020-07-11 14:54:29 +0000

    add poisson sampler

--- epsie/proposals/__init__.py
+++ epsie/proposals/__init__.py
@@ -27,6 +27,7 @@ from .discrete import (NormalDiscrete, SSAdaptiveNormalDiscrete,
                        AdaptiveNormalDiscrete,
                        BoundedDiscrete, SSAdaptiveBoundedDiscrete,
                        AdaptiveBoundedDiscrete)
+from .poisson import Poisson
 
 
 proposals = {
@@ -46,4 +47,5 @@ proposals = {
     BoundedDiscrete.name: BoundedDiscrete,
     SSAdaptiveBoundedDiscrete.name: SSAdaptiveBoundedDiscrete,
     AdaptiveBoundedDiscrete.name: AdaptiveBoundedDiscrete,
+    Poisson.name: Poisson,
 }
