7c6e7949e5fe9b40d51389ff49192358e22309dc
Richard-Sti <richard.stiskalek@protonmail.com>
2020-07-21 07:17:47 +0000

    testing and cleaning up the code

--- epsie/proposals/adaptcov_normal.py
+++ epsie/proposals/adaptcov_normal.py
@@ -18,8 +18,8 @@ from __future__ import (absolute_import, division)
 from abc import ABCMeta
 from six import add_metaclass
 
-import numpy as np
 from scipy import stats
+import numpy
 
 from .normal import Normal
 
@@ -34,7 +34,33 @@ class AdaptiveCovarianceSupport(object):
 
     Notes
     -----
-    add maths overview
+    Denoting :math: `g` to be the current samples, the new samples
+    :math: `x_{g+1}` are sampled from
+    :math: `\mathcal{N}\left(x_{g}, r^{2} \times C_{g}`. Where :math: `r` is
+    a scalar multipliying the covariance. For now :math: `r=1`. The covariance
+    is updates so that:
+
+    .. math::
+
+        C_{g+1} = C_{g} +  \gamma_{g+1}\left[\left(x_{g+1} - m_{g}\right)
+                  \left(x_{g+1} - m_{g}\right)^{T} - C_{g}\right],
+
+    where :math:`\gamma_{g+1}` is the vanishing decay and :math: `m_{g}` is a
+    supporting vector that is also being updated at each turn:
+
+    .. math::
+
+        m_{g+1} = m_{g} + \gamma_{g+1}\left(x_{g+1} - m_{g}\right).
+
+    By default the following relation is used for the vanishing decay:
+
+    .. math::
+        \gamma_{g+1} = \left(g - g_{0}\right)^{-0.2} - C,
+
+    where :math: `g_{0}` is the iteration at which adaptation starts,
+    by default :math: `g_{0}=1` and :math: `C` is a positive constant
+    ensuring that when the adaptation phase ends the vanishing decay tends to
+    zero.
 
     References
     ----------
@@ -43,124 +69,85 @@ class AdaptiveCovarianceSupport(object):
         https://mosaic.mpi-cbg.de/docs/Muller2010b.pdf
     """
 
-    _prior_widths = None
-    _deltas = None
-    _adaptation_duration = None
-    _start_iteration = None
-    target_rate = None
+    _adapt_dur = None
+    _start_iter = None
 
-    def setup_adaptation(self, prior_widths, adaptation_duration,
-                         start_iteration=1, target_rate=0.234,
-                         initial_mean=None, initial_cov=None):
+    def setup_adaptation(self, adapt_dur, start_iter=1):
         r"""Sets up the adaptation parameters.
+        adapt_dur : int
+            The number of iterations over which to apply the adaptation. No more
+            adaptation will be done once a chain exceeds this value.
+        start_iter: int (optional)
+            The iteration index when adaptation phase begins.
 
         Parameters
         ----------
         """
-        self.prior_widths = prior_widths
-        self.adaptation_duration = adaptation_duration
-        self.start_iteration = start_iteration
-
-        self.target_rate = target_rate
-        # Set up the initial mean
-        if initial_mean is None:
-            # update this to be in the middle of the bounds
-            initial_mean = np.array([5, 3])
+        self.adapt_dur = adapt_dur
+        self.start_iter = start_iter
+        ndim = len(self.parameters)
         # Set the mean to the initial
-        self._mean = initial_mean
+        self._mean = numpy.zeros(ndim)
         # Set up the initial covariance
-        if initial_cov is None:
-            deltas = np.array([self.prior_widths[p] for p in self.parameters])
-            initial_cov = (np.eye(len(self.prior_widths))
-                           * (1 - self.target_rate) * 0.09 * deltas)
-        # set the covariance to the initial
-        self._cov = initial_cov
+        self._cov = numpy.eye(ndim)
         # this will later be used to achieve target acceptance fraction
-        # for now keep unity
         self._r = 1
-
-        self._adaptation_end = self.start_iteration + self.adaptation_duration
-
-    @property
-    def prior_widths(self):
-        """The width of the prior used for each parameter."""
-        return self._prior_widths
-
-    @prior_widths.setter
-    def prior_widths(self, prior_widths):
-        """Sets the prior widths, making sure that widths are provided for
-        each parameter in ``parameters``.
-
-        Also sets the deltas attribute.
-        """
-        try:
-            self._prior_widths = {p: abs(prior_widths[p])
-                                  for p in self.parameters}
-        except KeyError:
-            raise ValueError("must provide prior widths for every parameter")
+        self._decay_const = (self.adapt_dur
+                            - self.start_iter - 1)**(-0.2)
 
     @property
-    def adaptation_duration(self):
+    def adapt_dur(self):
         """The adaptation duration used."""
-        return self._adaptation_duration
+        return self._adapt_dur
 
-    @adaptation_duration.setter
-    def adaptation_duration(self, adaptation_duration):
+    @adapt_dur.setter
+    def adapt_dur(self, adapt_dur):
         """Sets the adaptation duration to the given value, making sure it is
         larger than 1.
         """
-        if adaptation_duration < 1:
+        if adapt_dur < 1:
             raise ValueError("adaptation duration must be >= 1")
-        self._adaptation_duration = adaptation_duration
+        self._adapt_dur = adapt_dur
 
     @property
-    def start_iteration(self):
+    def start_iter(self):
         """The iteration that the adaption begins."""
-        return self._start_iteration
+        return self._start_iter
 
-    @start_iteration.setter
-    def start_iteration(self, start_iteration):
+    @start_iter.setter
+    def start_iter(self, start_iter):
         """Sets the start iteration, making sure it is >= 1."""
-        if start_iteration < 1:
-            raise ValueError("start_iteration must be >= 1")
-        self._start_iteration = start_iteration
+        if start_iter < 1:
+            raise ValueError("start_iter must be >= 1")
+        self._start_iter = start_iter
+
+    def decay(self, iteration):
+        """Adaptive decay to ensure vanishing adaptation. Later make this the
+        default choice and let user specify the functional form"""
+        return iteration**(-0.2) - self._decay_const
 
     def update(self, chain):
         """Updates the adaptation based on whether the last jump was accepted.
         This prepares the proposal for the next jump.
         """
 
-        if self.start_iteration < chain.iteration < self._adaptation_end:
-            # change this decay later. For now ok
-            decay = (chain.iteration - self.start_iteration)**(-0.2) - 0.1
-
-            # how to better unpack the last position?
-            names = chain.positions.dtype.names
-            lpos = chain.positions[-1]
-            newpt = np.array([lpos[p] for p in names])
+        if self.start_iter < chain.iteration < (self.start_iter
+                                                + self.adapt_dur) - 1:
+            decay = self.decay(chain.iteration)
+            newpt = numpy.array([chain.positions[-1][p]
+                                 for p in self.parameters])
             # Update the first and second moments
-            new_mean = self._mean + decay * (newpt - self._mean)
-            d = (newpt - self._mean).reshape(-1, 1)
-            new_cov = self._cov + decay * (np.matmul(d, d.T) - self._cov)
+            d = newpt - self._mean
+            new_mean = self._mean + decay * d
+            d = d.reshape(-1, 1)
+            new_cov = (self.cov + decay*(numpy.matmul(d, d.T) - self.cov))
 
-            # this is to ensure that the logpdf method does not throw an error
-            # sometimes it returns SIngularMatrix even if it can sample a rvs
-            # but the logpdf method fails. ANyway there has to be some check
-            # for singular matrices
-            try:
-                __ = stats.multivariate_normal.logpdf(newpt, mean=new_mean,
-                                                      cov=new_cov)
+            # Rough check that the new covariance is not singular due to
+            # numerical errors. Still would be better to start the specific
+            # exception that scipy.stats.multivariate_normal.logpdf throws
+            if numpy.linalg.det(new_cov) > 1e-7:
                 self._mean = new_mean
-                self._cov = self._r**2 * new_cov
-            except np.linalg.LinAlgError:
-                pass
-            # diagnostics
-#        if chain.iteration % 100 == 0:
-#            if chain.iteration > 10000:
-#                decay = (chain.iteration - self.start_iteration)**(-0.2) - 0.1
-#                print(chain.iteration, decay)
-#                print('mean', self._mean)
-#                print('cov', self._cov)
+                self.cov = self._r**2* new_cov
 
     @property
     def state(self):
@@ -174,7 +161,7 @@ class AdaptiveCovarianceSupport(object):
         self.random_state = state['random_state']
         self._mean = state['mean']
         self._cov = state['cov']
-        self._rv = state['r']
+        self._r = state['r']
 
 
 class AdaptiveCovarianceNormal(AdaptiveCovarianceSupport, Normal):
@@ -186,14 +173,10 @@ class AdaptiveCovarianceNormal(AdaptiveCovarianceSupport, Normal):
     ----------
     parameters : (list of) str
         The names of the parameters.
-    prior_widths : dict
-        Dictionary mapping parameter names to values giving the width of each
-        parameter's prior. The values may be floats, or any object that has
-        an ``__abs__`` method that will return a float.
-    adaptation_duration : int
+    adapt_dur : int
         The number of iterations over which to apply the adaptation. No more
         adaptation will be done once a chain exceeds this value.
-    start_iteration: int (optional)
+    start_iter: int (optional)
         The iteration index when adaptation phase begins.
     \**kwargs :
         All other keyword arguments are passed to
@@ -203,11 +186,10 @@ class AdaptiveCovarianceNormal(AdaptiveCovarianceSupport, Normal):
     name = 'adaptive_covariance_normal'
     symmetric = False
 
-    def __init__(self, parameters, prior_widths, adaptation_duration,
-                 start_iteration=1, **kwargs):
+    def __init__(self, parameters, adapt_dur,
+                 start_iter=1, **kwargs):
         # set the parameters, initialize the covariance matrix
         super(AdaptiveCovarianceNormal, self).__init__(parameters)
         # set up the adaptation parameters
-        self.setup_adaptation(prior_widths, adaptation_duration,
-                              start_iteration, **kwargs)
+        self.setup_adaptation(adapt_dur, start_iter, **kwargs)
         self._isdiagonal = False
