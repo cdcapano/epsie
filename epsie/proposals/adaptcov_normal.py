7ae049209168ba2cd084f231f055da6466ce879c
Richard-Sti <richard.stiskalek@protonmail.com>
2020-07-28 11:22:08 +0000

    add target acceptance ratio support

--- epsie/proposals/adaptcov_normal.py
+++ epsie/proposals/adaptcov_normal.py
@@ -28,7 +28,7 @@ from .normal import Normal
 class AdaptiveCovarianceSupport(object):
     r""" Utility class for adding adaptive covariance support to a proposal.
 
-    The adaptation algorithm is based on  Algorithm 1 in [1]. The proposal
+    The adaptation algorithm is based on  Algorithm 2 in [1]. The proposal
     covariance matrix is being adjusted at each step. See notes and [1] for
     more details
 
@@ -37,8 +37,7 @@ class AdaptiveCovarianceSupport(object):
     Denoting :math: `g` to be the current samples, the new samples
     :math: `x_{g+1}` are sampled from
     :math: `\mathcal{N}\left(x_{g}, r^{2} \times C_{g}`. Where :math: `r` is
-    a scalar multipliying the covariance. For now :math: `r=1`. The covariance
-    is updates so that:
+    a scalar multipliying the covariance.
 
     .. math::
 
@@ -52,10 +51,20 @@ class AdaptiveCovarianceSupport(object):
 
         m_{g+1} = m_{g} + \gamma_{g+1}\left(x_{g+1} - m_{g}\right).
 
-    By default the following relation is used for the vanishing decay:
+    The global scaling factor is updated to achieve a target acceptance ratio,
+    :math: `alpha^*`:
 
     .. math::
-        \gamma_{g+1} = \left(g - g_{0}\right)^{-0.2} - C,
+        \log\left(r_{g+1}\right) = \log\left(r_{g}\right)
+            + \gamma_{g+1}\left[\alpha\left(x_{g}, y_{g+1}\left)
+                                            - alpha^*\right)\right],
+
+    where :math `\alpha` is the Metropolis-Hastings ratio and :math `y_{g+1}`
+    is the next proposed step. By default the following relation is used
+    for the vanishing decay:
+
+    .. math::
+        \gamma_{g+1} = \left(g - g_{0}\right)^{-0.5} - C,
 
     where :math: `g_{0}` is the iteration at which adaptation starts,
     by default :math: `g_{0}=1` and :math: `C` is a positive constant
@@ -69,44 +78,26 @@ class AdaptiveCovarianceSupport(object):
         https://mosaic.mpi-cbg.de/docs/Muller2010b.pdf
     """
 
-    _adapt_dur = None
+    _target_acceptance = None
     _start_iter = None
 
-    def setup_adaptation(self, adapt_dur, start_iter=1):
+    def setup_adaptation(self, start_iter=1, target_acceptance=0.234):
         r"""Sets up the adaptation parameters.
-        adapt_dur : int
-            The number of iterations over which to apply the adaptation.
-            No more adaptation will be done once a chain exceeds this value.
         start_iter: int (optional)
             The iteration index when adaptation phase begins.
+        target_acceptance: float (optional)
+            Target acceptance ratio. By default 0.234
 
         Parameters
         ----------
         """
-        self.adapt_dur = adapt_dur
         self.start_iter = start_iter
+        self.target_acceptance = target_acceptance
         ndim = len(self.parameters)
-        # Set the mean to the initial
-        self._mean = numpy.zeros(ndim)
-        # Set up the initial covariance
-        self._cov = numpy.eye(ndim)
-        # this will later be used to achieve target acceptance fraction
-        self._r = 1
-        self._decay_const = (self.adapt_dur - self.start_iter - 1)**(-0.2)
+        self._mean = numpy.zeros(ndim) # initial mean
+        self._logr = 0.0 # initial global scaling
+        self._cov = numpy.eye(ndim) # inital covariance
 
-    @property
-    def adapt_dur(self):
-        """The adaptation duration used."""
-        return self._adapt_dur
-
-    @adapt_dur.setter
-    def adapt_dur(self, adapt_dur):
-        """Sets the adaptation duration to the given value, making sure it is
-        larger than 1.
-        """
-        if adapt_dur < 1:
-            raise ValueError("adaptation duration must be >= 1")
-        self._adapt_dur = adapt_dur
 
     @property
     def start_iter(self):
@@ -120,18 +111,27 @@ class AdaptiveCovarianceSupport(object):
             raise ValueError("start_iter must be >= 1")
         self._start_iter = start_iter
 
+    @property
+    def target_acceptance(self):
+        """Target acceptance ratio."""
+        return self._target_acceptance
+
+    @target_acceptance.setter
+    def target_acceptance(self, target_acceptance):
+        if not 0.0 < target_acceptance < 1.0:
+            raise ValueError("Target acceptance must be in range (0, 1)")
+        self._target_acceptance = target_acceptance
+
     def decay(self, iteration):
         """Adaptive decay to ensure vanishing adaptation. Later make this the
         default choice and let user specify the functional form"""
-        return iteration**(-0.2) - self._decay_const
+        return (iteration - self.start_iter + 1)**(-0.5)
 
     def update(self, chain):
         """Updates the adaptation based on whether the last jump was accepted.
         This prepares the proposal for the next jump.
         """
-
-        if self.start_iter < chain.iteration < (self.start_iter
-                                                + self.adapt_dur) - 1:
+        if self.start_iter < chain.iteration:
             decay = self.decay(chain.iteration)
             newpt = numpy.array([chain.positions[-1][p]
                                  for p in self.parameters])
@@ -141,16 +141,19 @@ class AdaptiveCovarianceSupport(object):
             d = d.reshape(-1, 1)
             new_cov = (self.cov + decay*(numpy.matmul(d, d.T) - self.cov))
 
+            alpha_mh = min(1, chain.acceptance['acceptance_ratio'][-1])
+            new_logr = self._logr\
+                       + decay * (alpha_mh - self.target_acceptance)
             try:
-                __ = stats.multivariate_normal(new_mean, new_cov)
+                r = numpy.exp(new_logr)
+                __ = stats.multivariate_normal(new_mean,
+                                               r**2 * new_cov).logpdf(newpt)
                 self._mean = new_mean
-                self.cov = self._r**2* new_cov
+                self.cov = r**2 * new_cov
+                self._logr = new_logr
+
             except numpy.linalg.LinAlgError:
-                # keep the old covariancec matrix
                 pass
-        else:
-            # after the adaptive phase the proposal becomes symmetric
-            self.symmetric = True
 
     @property
     def state(self):
@@ -175,11 +178,10 @@ class AdaptiveCovarianceNormal(AdaptiveCovarianceSupport, Normal):
     ----------
     parameters : (list of) str
         The names of the parameters.
-    adapt_dur : int
-        The number of iterations over which to apply the adaptation. No more
-        adaptation will be done once a chain exceeds this value.
     start_iter: int (optional)
         The iteration index when adaptation phase begins.
+    target_acceptance: float (optional)
+        Target acceptance ratio. By default 0.234
     \**kwargs :
         All other keyword arguments are passed to
         :py:func:`AdaptiveSupport.setup_adaptation`. See that function for
@@ -188,10 +190,10 @@ class AdaptiveCovarianceNormal(AdaptiveCovarianceSupport, Normal):
     name = 'adaptive_covariance_normal'
     symmetric = False
 
-    def __init__(self, parameters, adapt_dur,
-                 start_iter=1, **kwargs):
+    def __init__(self, parameters, start_iter=1, target_acceptance=0.234,
+                 **kwargs):
         # set the parameters, initialize the covariance matrix
         super(AdaptiveCovarianceNormal, self).__init__(parameters)
         # set up the adaptation parameters
-        self.setup_adaptation(adapt_dur, start_iter, **kwargs)
+        self.setup_adaptation(start_iter, target_acceptance, **kwargs)
         self._isdiagonal = False
