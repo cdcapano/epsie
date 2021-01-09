b88162e1a5b3f290ea263f90344ef31b38a8fc47
Collin Capano <collin.capano@ligo.org>
2021-01-09 16:49:33 +0100

    run scripts in _include in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -59,3 +59,15 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# run scripts in _include
+import glob
+import subprocess
+import os
+print("Running scripts in _include directory:")
+cwd = os.getcwd()
+os.chdir('_include')
+for fn in glob.glob("*.sh"):
+    print("  {}".format(fn))
+    subprocess.run(['sh', fn])
+os.chdir(cwd)
