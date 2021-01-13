82d57780a61b0df2b876349bcb81f7bf8cf3776d
Collin Capano <collin.capano@ligo.org>
2021-01-13 22:22:48 +0100

    fix typo in setup.py

--- setup.py
+++ setup.py
@@ -16,8 +16,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-"""
-setup.py file for PyCBC package
+"""setup.py file for epsie package
 """
 import setuptools
 
