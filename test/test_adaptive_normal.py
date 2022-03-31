69db11180042b694b040fa8d1e69a781890b5277
Collin Capano <collin.capano@aei.mpg.de>
2022-03-31 18:00:18 -0400

    try importing multiprocessing

--- test/test_adaptive_normal.py
+++ test/test_adaptive_normal.py
@@ -16,6 +16,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """Performs unit tests on the AdaptiveNormal proposals."""
 
+import multiprocessing
 import itertools
 import pytest
 import numpy
