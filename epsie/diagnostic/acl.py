e60f15e635f2def962bcf9ebd97377abe658fba9
Richard Stiskalek <richard.stiskalek@protonmail.com>
2022-04-05 15:25:51 +0200

    Add PT thinning and comments

--- epsie/diagnostic/acl.py
+++ epsie/diagnostic/acl.py
@@ -13,16 +13,32 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-
-"""
-Autocorrelation time calculation. 1D autocorrelation code inspired from
-https://emcee.readthedocs.io/en/stable/tutorials/autocorr/#autocorr.
-"""
-
+"""Autocorrelation time calculation."""
 import numpy
 
 
 def acl_1d(x, c=5.0):
+    """
+    Calculate the autocorrelation length (ACL) of some series.
+    
+    Algorithm used is from:
+    N. Madras and A.D. Sokal, J. Stat. Phys. 50, 109 (1988).
+
+    Implementation from:
+    https://emcee.readthedocs.io/en/stable/tutorials/autocorr/#autocorr
+
+    Arguments
+    ---------
+    x : 1-dimensional array
+        Samples whose autocorrelation length is to be calculated.
+    c : float, optional
+        ACL hyperparameter. As per Sokal by default set to 5.
+    
+    Returns
+    -------
+    ACL: int
+        Autocorrelation length.
+    """
     if x.ndim != 1:
         raise TypeError("``x`` must be a 1D array. Currently {}".format(x.ndim))
     # n is the next power of 2 for length of x
@@ -35,12 +51,14 @@ def acl_1d(x, c=5.0):
     
     taus = 2.0 * numpy.cumsum(acf) - 1.0
     window = auto_window(taus, c)
-#    print(taus[window])
-    return numpy.round(taus[window]).astype(int)
+    return numpy.ceil(taus[window]).astype(int)
 
 def auto_window(taus, c):
     """
-    Automated windowing procedure following Sokal (1989)
+    ACL automated windowing procedure following Sokal (1989).
+
+    Implementation from:
+    https://emcee.readthedocs.io/en/stable/tutorials/autocorr/#autocorr
     """
     m = numpy.arange(len(taus)) < c * taus
     if numpy.any(m):
@@ -49,6 +67,26 @@ def auto_window(taus, c):
 
 
 def acl_chain(chain, burnin_iter=0, c=5.0, full=False):
+    """
+    Calculate the ACL of :py:class:`epsie.chain.Chain` chain.
+
+    Arguments
+    ---------
+    chain : :py:class:`epsie.chain.Chain`
+        Epsie chain.
+    burnin_iter : int, optional
+        Number of burnin iterations to be thrown away.
+    c : float, optional
+        ACL calculation hyperparameter. By default 5.
+    full : bool, optionall
+        Whether to return the ACL of every parameter. By default False, thus
+        returning only the maximum ACL.
+    
+    Returns
+    -------
+    ACL : int or array of integers
+        Chain's ACL(s).
+    """
     acls = [acl_1d(chain.positions[p][burnin_iter:]) for p in chain.parameters]
     if full:
         return acls
@@ -56,26 +94,86 @@ def acl_chain(chain, burnin_iter=0, c=5.0, full=False):
 
 
 def thinned_samples(sampler, burnin_iter=0, c=5.0):
+    """
+    Parse a sampler and return its samples thinned by the ACL.
+
+    Arguments
+    ---------
+    sampler : {:py:class:`epsie.sampler.MetropolisHastingsSampler`,
+               :py:class:`epsie.sampler.ParallelTemperedSampler`}
+        Epsie sampler whose samples to extract.
+    burnin_iter : int, optional
+        Number of burnin iterations to be thrown away.
+    c : float, optional
+        ACL calculation hyperparameter. By default 5.
+
+    Returns
+    -------
+    thinned samples : structured array
+        Array whose `dtype.names` are the sampler parameters. Each parameter's
+        data is 1-dimensional for the MH sampler and 2-dimensional for the PT
+        sampler. In the latter case the first and second axis represent the
+        temperature and sample index.
+    """
     if sampler.name == "mh_sampler":
-        return _thinned_mh_samples(sampler)
+        return _thinned_mh_samples(sampler, burnin_iter, c)
     elif sampler.name == "pt_sampler":
-        raise NotImplementedError("PT thinning not implemented yet")
+        return _thinned_pt_samples(sampler, burnin_iter, c)
     else:
         return ValueError("Invalid sampler kind.")
 
-def _thinned_mh_samples(sampler, burnin_iter=0, c=5.0):
-    params = sampler.chains[0].parameters
-    _thinned = {p: [] for p in params}
-    positions = sampler.positions[:, burnin_iter:]
 
-    # cycle over the chains, calculating the ACLs and thinning them
-    for i, chain in enumerate(sampler.chains):
-        acl = acl_chain(chain, burnin_iter, c)
+def _thinned_mh_samples(sampler, burnin_iter=0, c=5.0):
+    # Calculate the ACL for each chain 
+    acls = [acl_chain(chain, burnin_iter, c) for chain in sampler.chains]
+
+    params = sampler.parameters
+    _thinned = {p: [] for p in params}
+    # Explicitly cut off the burnin iterations
+    samples = sampler.positions[:, burnin_iter:]
+
+    # Cycle over the chains and thin them
+    for ii, chain in enumerate(sampler.chains):
         for p in params:
-            _thinned[p].append(positions[p][i, :][::-1][::acl][::-1])
-    
+            _thinned[p].append(samples[p][ii, :][::-1][::acls[ii]][::-1])
+    # Put the thinned samples into a structured array
     N = sum(x.size for x in _thinned[params[0]])
-    thinned = numpy.zeros(N, dtype=positions.dtype)
+    thinned = numpy.zeros(N, dtype=samples.dtype)
     for p in params:
         thinned[p] = numpy.concatenate(_thinned[p])
     return thinned
+
+
+def _thinned_pt_samples(sampler, burnin_iter=0, c=5.0):    
+
+    # Calculate the ACL across temperatures for each chain    
+    temp_acls = numpy.zeros((sampler.nchains, sampler.ntemps), dtype=int)
+    for ii in range(sampler.nchains):
+        for jj in range(sampler.ntemps):
+            temp_acls[ii, jj] = acl_chain(sampler.chains[ii].chains[jj], burnin_iter, c=0.5)
+    # Grab the maximum ACL along each each. Is there a better choice though?
+    acls = numpy.max(temp_acls, axis=1)
+
+    params = sampler.parameters
+    samples = sampler.positions[:, :, burnin_iter:]
+
+    thinned = {p: [] for p in params}
+    # Cycle over the chains, thinning them
+    for tk in range(sampler.ntemps):
+        _thinned = {p: [] for p in params}
+
+        for ii in range(sampler.nchains):
+            for param in params:
+                sp = samples[param][tk, ii, :]
+                _thinned[param].append(sp[::-1][::acls[ii]][::-1])
+        
+        for param in params:
+            thinned[param].append(numpy.concatenate(_thinned[param]))
+    
+    # Cast the thinned samples from a list of arrays into a 2D array for each
+    # parameter and return as a structured array
+    thinned_samples = numpy.zeros((sampler.ntemps, thinned[params[0]][0].size),
+                                  dtype=samples.dtype)
+    for param in params:
+        thinned_samples[param] = numpy.asarray(thinned[param])
+    return thinned_samples
