db1d0a73e279554937f6556cbb6c569deafdc0a9
Richard Stiskalek <richard.stiskalek@protonmail.com>
2022-04-12 11:01:12 +0200

    add a setter for num of dragging steps

--- epsie/chain/fastchain.py
+++ epsie/chain/fastchain.py
@@ -38,51 +38,58 @@ class FastChain(Chain):
     """
     _is_fast = True
 
-    def __init__(self, parameters, model, proposals, slow_parameters, nfast, bit_generator=None):
-        # Initialise the parent chain
-        # Add a setter for this
-        self.nfast = nfast
-
-        # Make sure there is no overlap between parameters and slow_parameters
-        self.slow_parameters = slow_parameters
-
+    def __init__(self, parameters, model, proposals, slow_parameters, ndrags,
+                 bit_generator=None):
         self._current_slow = None
         self._proposed_slow = None
+        self._ndrags = None
 
+        self.slow_parameters = slow_parameters
+        self.ndrags = ndrags
+
+        # Must be renamed because we will pass ``self.model`` to the parent
+        # chain instead
         self.original_model = model
 
+        # To store the likelihoods when evaluated with fixed slow and changing
+        # fast parameters. Necessary because by default the chain is storing
+        # only the interpolated probabilities.
         self._dragged_stats = ChainData(["lpost0", "lpostf"])
-        self._dragged_stats.set_len(nfast)
+        self._dragged_stats.set_len(ndrags)
 
-
+        # Initialise the parent chain
         super().__init__(parameters, self.model, proposals, bit_generator,
                          chain_id=0, beta=1.)
 
 
     @property
     def current_slow(self):
+        """Current slow parameters."""
         if self._current_slow is None:
             raise ValueError("``current_slow`` not set!")
         return self._current_slow
 
     @current_slow.setter
     def current_slow(self, current_slow):
+        """Sets the current fast parameters."""
         self._current_slow = self._set_slow_params(current_slow)
 
     @property
     def proposed_slow(self):
+        """Proposed slow parameters."""
         if self._proposed_slow is None:
             raise ValueError("``proposed_slow`` not set!")
         return self._proposed_slow
 
     @proposed_slow.setter
     def proposed_slow(self, proposed_slow):
+        """Sets the proposed slow parameters."""
         self._proposed_slow = self._set_slow_params(proposed_slow)
 
     def _set_slow_params(self, positions):
         """
-        Check that parameters in `self.slow_parameters` 
-
+        Check that slow parameters in ``positions`` match the expected slow
+        parameters and that no extra parameters are given.
         """
         if positions is None:
             return None
@@ -101,6 +108,18 @@ class FastChain(Chain):
 
         return checked_positions
 
+    @property
+    def ndrags(self):
+        """Number of fast parameter dragging step."""
+        return self._ndrags
+
+    @ndrags.setter
+    def ndrags(self, ndrags):
+        """Sets the number of dragging steps."""
+        if not ndrags > 1:
+            raise ValueError("`ndrags` must be larger than 1.")
+        self._ndrags = ndrags
+
     def model(self, **proposed):
         """
         Interpolated posterior between the previous and proposed slow
@@ -108,7 +127,7 @@ class FastChain(Chain):
         of completed fast steps.
         """
         # Fraction of completed fast steps, + 1 to account for the start pos
-        prog = (self.iteration + 1) / self.nfast
+        prog = (self.iteration + 1) / self.ndrags
         if prog > 1:
             raise ValueError("Chain should have been cleared.")
         # Call the models with the slow and fast parameters
@@ -149,7 +168,7 @@ class FastChain(Chain):
 
     def dragging_clear(self):
         self.clear()  # clear the parent chain
-        self._dragged_stats.clear(self.nfast)  # clear the dragged stats
+        self._dragged_stats.clear(self.ndrags)  # clear the dragged stats
         self._lastclear = 0  # we don't want to use this
         # Reset the iterations
         self._iteration = 0
@@ -161,5 +180,5 @@ class FastChain(Chain):
         """
         Util function to test the chain, will later remove. 
         """
-        for __ in range(1, self.nfast):
+        for __ in range(1, self.ndrags):
             self.step()
