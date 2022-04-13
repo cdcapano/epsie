1733d0c157a7c23df608703a779677236519ab6e
Richard Stiskalek <richard.stiskalek@protonmail.com>
2022-04-13 10:18:46 +0200

    __init__ import swap decay

--- epsie/chain/__init__.py
+++ epsie/chain/__init__.py
@@ -17,4 +17,4 @@
 # promote chain classes to module namespace
 from .chaindata import ChainData
 from .chain import Chain
-from .ptchain import (ParallelTemperedChain, DynamicalAnnealer)
+from .ptchain import (ParallelTemperedChain, DynamicalAnnealer, BasicSwapDecay)
